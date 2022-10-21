e89495c72a3308f71e4bdbd3721a201551e081f3
Wahyu <wahyukoco535@gmail.com>
2022-10-21 14:44:28 +0700

    splash screen setup

--- app/src/main/java/com/example/petcare/MainActivity.kt
+++ app/src/main/java/com/example/petcare/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.petcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
