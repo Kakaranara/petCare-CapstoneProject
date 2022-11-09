674dc3c000ced8dd76ca086d0bcb0ea75c885dcc
Wahyu <wahyukoco535@gmail.com>
2022-11-09 07:34:55 +0700

    activity setup

--- app/src/main/java/com/example/petcare/MainActivity.kt
+++ app/src/main/java/com/example/petcare/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.petcare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.petcare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splash = installSplashScreen() // !required for splash screen
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
