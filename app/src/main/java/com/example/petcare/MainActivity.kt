bf50ed99770977ca302928ae485ca24101e480f1
Wahyu <wahyukoco535@gmail.com>
2022-11-12 19:47:07 +0700

    bottom navigation setup

--- app/src/main/java/com/example/petcare/MainActivity.kt
+++ app/src/main/java/com/example/petcare/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.petcare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.petcare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //? setup for bottom navigation bar | same menu id & fragment = destination for bottom bar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavbar.setupWithNavController(navController)
     }
 }
