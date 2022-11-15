b5b71d29b5f28853bad7b866c1a142af197ab6ec
Wahyu <wahyukoco535@gmail.com>
2022-11-15 13:49:14 +0700

    feat: register validation

--- app/src/main/java/com/example/petcare/ui/user/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/petcare/ui/user/auth/register/RegisterFragment.kt
@@ -9,8 +9,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.petcare.databinding.FragmentRegisterBinding
+import com.example.petcare.utils.AuthUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment(), View.OnClickListener {
@@ -34,37 +36,54 @@ class RegisterFragment : Fragment(), View.OnClickListener {
                 findNavController().navigate(go)
             }
             binding.btnRegister -> {
-                binding.btnRegister.isEnabled = false
-                binding.registerProgress.visibility = View.VISIBLE
+                val name = binding.etName.text.toString()
                 val email = binding.etRegisterEmail.text.toString()
                 val password = binding.etRegisterPassword.text.toString()
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(requireActivity()) { task ->
-                        if (task.isComplete) {
-                            binding.btnRegister.isEnabled = true
-                            binding.registerProgress.visibility = View.INVISIBLE
-                        }
-                        if (task.isSuccessful) {
-                            Toast.makeText(
-                                requireActivity(),
-                                "Account successfully created.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            Log.d(TAG, "onClick: Login success.")
-                            val go =
-                                RegisterFragmentDirections.actionGlobalLoginFragment()
-                            findNavController().navigate(go)
-                        } else {
-                            Toast.makeText(
-                                requireActivity(),
-                                "Failed. ${task.exception}",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            Log.d(TAG, "onClick: Fail login. Exception : ${task.exception}")
-                        }
-                    }
+                val confirmPassword = binding.etRegisterConfirmPassword.text.toString()
+                try {
+                    AuthUtil.isRegisterValid(email, password,confirmPassword, name)
+                    registerUser(email, password, name)
+                } catch (e: Exception) {
+                    Toast.makeText(requireActivity(), e.message, Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    private fun registerUser(email: String, password: String, name: String) {
+        binding.btnRegister.isEnabled = false
+        binding.registerProgress.visibility = View.VISIBLE
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isComplete) {
+                    binding.btnRegister.isEnabled = true
+                    binding.registerProgress.visibility = View.INVISIBLE
+                }
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val profileUpdate = userProfileChangeRequest {
+                        displayName = name
+                    }
+                    user?.updateProfile(profileUpdate)
+                    Toast.makeText(
+                        requireActivity(),
+                        "Account successfully created.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d(TAG, "onClick: Login success.")
+                    val go =
+                        RegisterFragmentDirections.actionGlobalLoginFragment()
+                    findNavController().navigate(go)
+                } else {
+                    Toast.makeText(
+                        requireActivity(),
+                        "${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d(TAG, "onClick: Fail login. Exception : ${task.exception}")
+                }
+            }
     }
 
     override fun onCreateView(
--- app/src/main/java/com/example/petcare/utils/Utils.kt
+++ app/src/main/java/com/example/petcare/utils/Utils.kt
@@ -10,4 +10,13 @@ object AuthUtil {
             throw Exception("Mail and Password should not be empty!")
         }
     }
+
+    fun isRegisterValid(email: String, password: String, confirmPassword: String, name: String) {
+        if (email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+            throw Exception("Please input all the field!")
+        }
+        if (password != confirmPassword) {
+            throw Exception("Password did not match")
+        }
+    }
 }
