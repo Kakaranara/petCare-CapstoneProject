ad4265747b958b35fdd29f31150ea7af87e6980a
Wahyu <wahyukoco535@gmail.com>
2022-11-25 12:16:56 +0700

    feat(profile) : edit profile functionality (Mvp)

--- app/src/main/java/com/example/petcare/ui/user/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/petcare/ui/user/profile/EditProfileFragment.kt
@@ -1,15 +1,19 @@
 package com.example.petcare.ui.user.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.example.petcare.databinding.FragmentEditProfileBinding
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
@@ -17,6 +21,21 @@ import com.google.firebase.ktx.Firebase
 class EditProfileFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
+    private var uri: Uri? = null
+
+    private val intentGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                uri = result.data?.data
+                Glide.with(requireActivity())
+                    .load(uri)
+                    .into(binding.circleImageEdit)
+            }
+        }
+
+    companion object {
+        const val TAG = "Edit Profile Fragment"
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,6 +44,7 @@ class EditProfileFragment : Fragment(), View.OnClickListener {
         binding.btnConfirmEdit.setOnClickListener(this)
 
         Firebase.auth.currentUser?.let { user ->
+            uri = user.photoUrl
             Glide.with(requireActivity())
                 .load(user.photoUrl)
                 .into(binding.circleImageEdit)
@@ -35,12 +55,22 @@ class EditProfileFragment : Fragment(), View.OnClickListener {
     override fun onClick(view: View) {
         when (view) {
             binding.btnEditImage -> {
-                Snackbar.make(requireView(), "TEST", Snackbar.LENGTH_SHORT).show()
+                Intent(Intent.ACTION_GET_CONTENT).apply {
+                    type = "image/*"
+                }.also { intentGallery.launch(Intent.createChooser(it, "im here bro")) }
             }
             binding.btnConfirmEdit -> {
                 val update = userProfileChangeRequest {
                     displayName = binding.etEditName.text.toString()
-                    //TODO : update image from gallery
+                    photoUri = uri
+                }
+                Firebase.auth.currentUser?.let {
+                    it.updateProfile(update).addOnSuccessListener {
+                        Log.d("edit profile", "onClick: update success")
+                        findNavController().popBackStack()
+                    }.addOnFailureListener { e ->
+                        Log.d("edit profile", "onClick: failed $e ")
+                    }
                 }
             }
         }
