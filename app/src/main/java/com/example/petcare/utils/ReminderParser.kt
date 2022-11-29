5702ca33c3eb7d2cb27b533c757666fbf9252bc5
Wahyu <wahyukoco535@gmail.com>
2022-11-29 09:40:55 +0700

    refactor a bit

--- app/src/main/java/com/example/petcare/utils/ReminderParser.kt
+++ app/src/main/java/com/example/petcare/utils/ReminderParser.kt
@@ -1,31 +1,37 @@
 package com.example.petcare.utils
 
 object ReminderParser {
+
+    val second: Long = 1 * 1000
+    val minute: Long = second * 60
+    val hour: Long = minute * 60
+    val day: Long = hour * 24
+
     fun getAdditionalTime(remindBefore: String): Long {
         return when (remindBefore) {
             "15 Minutes" -> {
-                15 * 1000 * 60
+                15 * minute
             }
             "30 Minutes" -> {
-                30 * 1000 * 60
+                30 * minute
             }
             "1 Hours" -> {
-                1 * 1_000 * 60 * 60
+                1 * hour
             }
 
             "2 Hours" -> {
-                2 * 1_000 * 60 * 60
+                2 * hour
             }
 
             "4 Hours" -> {
-                4 * 1_000 * 60 * 60
+                4 * hour
             }
             "8 Hours" -> {
-                8 * 1_000 * 60 * 60
+                8 * hour
             }
 
             "1 Days" -> {
-                24 * 1_000 * 60 * 60
+                1 * day
             }
 
             else -> 0
