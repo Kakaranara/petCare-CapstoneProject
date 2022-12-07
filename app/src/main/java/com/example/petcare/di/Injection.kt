1a8924bfda245ae2a5faf47b8d03959d51184cd0
ribakmasude <beranjusihombing1@gmail.com>
2022-12-07 15:45:45 +0700

    update user

--- app/src/main/java/com/example/petcare/di/Injection.kt
+++ app/src/main/java/com/example/petcare/di/Injection.kt
@@ -1,11 +1,6 @@
 package com.example.petcare.di
 
 import com.example.petcare.data.StoryRepository
-import com.example.petcare.data.repository.ProfileRepository
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -15,11 +10,5 @@ object Injection {
         val rooRef : FirebaseFirestore = FirebaseFirestore.getInstance()
         val mStorage: StorageReference = FirebaseStorage.getInstance().reference
         return StoryRepository(rooRef, mStorage)
-    }
-    fun provideProfileRepository(): ProfileRepository{
-        val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
-        val rooRef : FirebaseFirestore = FirebaseFirestore.getInstance()
-        val storageRef: StorageReference = FirebaseStorage.getInstance().reference
-        return ProfileRepository(mAuth, rooRef, storageRef)
     }
 }
