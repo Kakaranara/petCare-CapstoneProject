5acd17cdb6988600fd539ed8d725d16c81ef7f91
ribakmasude <beranjusihombing1@gmail.com>
2022-12-07 16:23:14 +0700

    fix typo and error at strings and injection

--- app/src/main/java/com/example/petcare/di/Injection.kt
+++ app/src/main/java/com/example/petcare/di/Injection.kt
@@ -7,7 +7,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.CollectionReference 
 import com.example.petcare.data.remote.retrofit.ApiConfig
-import com.example.petcare.data.repository.PetShopRepository 
+import com.example.petcare.data.repository.PetShopRepository
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -17,15 +17,10 @@ object Injection {
         val rooRef : FirebaseFirestore = FirebaseFirestore.getInstance()
         val mStorage: StorageReference = FirebaseStorage.getInstance().reference
         return StoryRepository(rooRef, mStorage)
-    } 
-    fun provideProfileRepository(): ProfileRepository{
-        val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
-        val rooRef : FirebaseFirestore = FirebaseFirestore.getInstance()
-        val storageRef: StorageReference = FirebaseStorage.getInstance().reference
-        return ProfileRepository(mAuth, rooRef, storageRef) 
+    }
 
     fun providePetShopRepository(): PetShopRepository {
         val apiService = ApiConfig.getApiService()
-        return PetShopRepository(apiService) 
+        return PetShopRepository(apiService)
     }
 }
