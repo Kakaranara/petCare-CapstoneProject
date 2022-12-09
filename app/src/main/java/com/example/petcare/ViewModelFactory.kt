263276df12af91eb2621fad5ce1b83a60cdebea7
Wahyu <wahyukoco535@gmail.com>
2022-12-09 12:20:49 +0700

    fix(bug) : fixing bug after merging from story feat

--- app/src/main/java/com/example/petcare/ViewModelFactory.kt
+++ app/src/main/java/com/example/petcare/ViewModelFactory.kt
@@ -12,23 +12,24 @@ import com.example.petcare.ui.main.story.main.StoryViewModel
 import com.example.petcare.ui.main.story.profile.ProfileViewModel
 
 @Suppress("UNCHECKED_CAST")
-class ViewModelFactory(private val repository: StoryRepository):ViewModelProvider.Factory {
+class ViewModelFactory(private val repository: StoryRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(StoryViewModel::class.java)){
+        if (modelClass.isAssignableFrom(StoryViewModel::class.java)) {
             return StoryViewModel(repository) as T
         }
-        if (modelClass.isAssignableFrom(AddStoryViewModel::class.java)){
+        if (modelClass.isAssignableFrom(AddStoryViewModel::class.java)) {
             return AddStoryViewModel(repository) as T
         }
-        if (modelClass.isAssignableFrom(CommentViewModel::class.java)){
+        if (modelClass.isAssignableFrom(CommentViewModel::class.java)) {
             return CommentViewModel(repository) as T
         }
-        if (modelClass.isAssignableFrom(DetailViewModel::class.java)){
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
             return DetailViewModel(repository) as T
         }
-        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)){
+        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
             return ProfileViewModel(repository) as T
-        if (modelClass.isAssignableFrom(PetShopViewModel::class.java)){
+        }
+        if (modelClass.isAssignableFrom(PetShopViewModel::class.java)) {
             return PetShopViewModel(Injection.providePetShopRepository()) as T
         }
         return super.create(modelClass)
--- app/src/main/java/com/example/petcare/di/Injection.kt
+++ app/src/main/java/com/example/petcare/di/Injection.kt
@@ -2,12 +2,6 @@ package com.example.petcare.di
 
 
 import com.example.petcare.data.StoryRepository
-import com.example.petcare.data.StoryRepository 
-import com.example.petcare.data.repository.ProfileRepository
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.firestore.CollectionReference 
 import com.example.petcare.data.remote.retrofit.ApiConfig
 import com.example.petcare.data.repository.PetShopRepository
 import com.google.firebase.firestore.FirebaseFirestore
